ff2100b5fdf9be02cdd9f0245a8563cb45383215
Kari Lavikka <kari.lavikka@bdb.fi>
2023-05-04 13:29:15 +0300

    chore: update vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     plugins: [minifyTemplateLiterals()],
     base: "./",
     build: {
+        emptyOutDir: true,
         rollupOptions: {},
         outDir: "../dist",
     },
